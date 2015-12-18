29ac9683d99feb607d3d459f7fd3a3e1d1dd0ca6
Mark Stoehr <stoehr.mark@gmail.com>
2015-12-18 02:14:12 -0600

    batchnorm operations

---
+++ lstm_acoustic_embedding/ops.py
@@ -0,0 +1,35 @@
+import theano
+from theano import tensor
+
+def batchnorm(X, g=None, b=None, u=None, s=None, a=1., e=1e-8):
+    """
+    batchnorm with support for not using scale and shift parameters
+    as well as inference values (u and s) and partial batchnorm (via a)
+    will detect and use convolutional or fully connected version
+    """
+    if X.ndim == 4:
+        if u is not None and s is not None:
+            b_u = u.dimshuffle('x', 0, 'x', 'x')
+            b_s = s.dimshuffle('x', 0, 'x', 'x')
+        else:
+            b_u = tensor.mean(X, axis=[0, 2, 3]).dimshuffle('x', 0, 'x', 'x')
+            b_s = tensor.mean(tensor.sqr(X - b_u), axis=[0, 2, 3]).dimshuffle('x', 0, 'x', 'x')
+        if a != 1:
+            b_u = (1. - a)*0. + a*b_u
+            b_s = (1. - a)*1. + a*b_s
+        X = (X - b_u) / tensor.sqrt(b_s + e)
+        if g is not None and b is not None:
+            X = X*g.dimshuffle('x', 0, 'x', 'x') + b.dimshuffle('x', 0, 'x', 'x')
+    elif X.ndim == 2:
+        if u is None and s is None:
+            u = tensor.mean(X, axis=0)
+            s = tensor.mean(tensor.sqr(X - u), axis=0)
+        if a != 1:
+            u = (1. - a)*0. + a*u
+            s = (1. - a)*1. + a*s
+        X = (X - u) / tensor.sqrt(s + e)
+        if g is not None and b is not None:
+            X = X*g + b
+    else:
+        raise NotImplementedError
+    return X
